605479b091740bfd5478f292085532ceb92899cb
Callum Brennan <callum.brennan@hotmail.co.uk>
2017-09-02 18:34:14 +0100

    Revert "no message"
    This reverts commit 0a9727c61879038c06364223c554a0e7832f0c84.

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -7,83 +7,19 @@
 //
 
 import UIKit
-import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController {
 
-    @IBOutlet weak var mapView: MKMapView!
-    
-    var updateCount = 0
-    
-    var manager = CLLocationManager()
-    
-<<<<<<< HEAD
-    var pokemons : [Pokemon] = []
-    
-    
-=======
->>>>>>> c8eac41d63194e034396e8b69c5119bcd1af1c2a
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-<<<<<<< HEAD
-        pokemons = getAllPokemon()
-        
-=======
->>>>>>> c8eac41d63194e034396e8b69c5119bcd1af1c2a
-        manager.delegate = self
-        
-       
-        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
-            print ("Ready To Go")
-            mapView.showsUserLocation = true
-            manager.startUpdatingLocation()
-            
-           Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
-            // Spawn Pokemon
-            
-            if let coord = self.manager.location?.coordinate {
-                let anno = MKPointAnnotation()
-                anno.coordinate = coord
-                let randolat = (Double(arc4random_uniform(200)) - 100.0)/50000.0
-                let randolon = (Double(arc4random_uniform(200)) - 100.0)/50000.0
-                anno.coordinate.latitude += randolat
-                anno.coordinate.longitude += randolon
-                self.mapView.addAnnotation(anno)
-            }
-            
-           })
-            
-        } else {
-            manager.requestWhenInUseAuthorization()
-        }
-    
-    
     }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        if updateCount < 3 {
-        let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 200, 200)
-        
-        mapView.setRegion(region, animated: false)
-            updateCount += 1
-        } else {
-            manager.stopUpdatingLocation()
-        }
-    }
-  
-    @IBAction func centerTapped(_ sender: Any) {
-        if let coord = manager.location?.coordinate {
-            
-            let region = MKCoordinateRegionMakeWithDistance(coord, 200, 200)
-            mapView.setRegion(region, animated: true)
-        }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-        
+
+
 }
 
-
-    
